f3179187bf936b95bd86669c3b1af72b29e4b1c9
王宇超 <wangyuchaowanglei@163.com>
2017-10-13 11:04:37 +0800

    创建空项目

---
+++ WYCTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYCTextView
+//
+//  Created by WD_王宇超 on 2017/10/13.
+//  Copyright © 2017年 WD_王宇超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WYCTextView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WYCTextView
+//
+//  Created by WD_王宇超 on 2017/10/13.
+//  Copyright © 2017年 WD_王宇超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
